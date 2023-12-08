5f38cfcab3bdb3bb573d1900f900a3851f4a1fb5
ddmrc <demarcosound@gmail.com>
2023-12-08 13:44:35 +0100

    client model

--- src/main/java/com/ddcsoftware/Main.java
+++ src/main/java/com/ddcsoftware/Main.java
@@ -5,11 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public Greet greet() {
+        String[] s = {"dpg", "cat", "fish"};
+        return new Greet("hello", 21, s);
+    }
+
+    public record Greet(String item, int age, String[] city) {
+
     }
 
 }
---
+++ src/main/java/com/ddcsoftware/models/Client.java
@@ -0,0 +1,82 @@
+package com.ddcsoftware.models;
+
+import java.util.Objects;
+
+public class Client {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Client() {
+        this.id = 0;
+        this.name = "N/A";
+        this.email = "N/A";
+        this.age = 0;
+    }
+
+    public Client(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) &&
+                Objects.equals(name, client.name) &&
+                Objects.equals(email, client.email) &&
+                Objects.equals(age, client.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
