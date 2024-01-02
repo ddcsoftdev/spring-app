be34fc553aaffcd0797d458d77820b35756b92cb
ddcsoftdev <ddcsoftwaredev@gmail.com>
2024-01-02 14:09:19 +0100

    decouple backend

---
+++ backend/src/main/java/com/ddcsoftware/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,102 @@
+package com.ddcsoftware.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                """;
+        return jdbcTemplate.query(sql, customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer customerId) {
+        var sql = """
+                SELECT * FROM customer
+                WHERE id = ?
+                """;
+        //return a list with stream and then findFirst.
+        //this is the fast way and oneliner method to check if empty,
+        // and get without throwing exception with get(0)
+        return jdbcTemplate.query(sql, customerRowMapper, customerId).stream().findFirst();
+    }
+
+    @Override
+    public boolean existsCustomerWithEmail(String email) {
+        var sql = """
+                SELECT count(*)
+                FROM customer
+                WHERE email = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+        return count != null && count > 0;
+    }
+
+    @Override
+    public boolean existsCustomerWithId(Integer customerId) {
+        var sql = """
+                SELECT count(*)
+                FROM customer
+                WHERE id = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, customerId);
+        return count != null && count > 0;
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        var sql = """
+                INSERT INTO customer (name, email, age)
+                VALUES (?, ?, ?)
+                """;
+
+        //returns number of rows affected
+        int rowsAffected = jdbcTemplate.update(sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge());
+        System.out.printf("Insert Customer, rowsAffected = %d\n", rowsAffected);
+    }
+
+    @Override
+    public void deleteCustomer(Integer customerId) {
+        var sql = """
+                DELETE FROM customer
+                WHERE id = ?
+                """;
+        int rowsAffected = jdbcTemplate.update(sql, customerId);
+        System.out.printf("Delete Customer by Id, rowsAffected = %d\n", rowsAffected);
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+        var sql = """
+                UPDATE customer
+                SET name = ? , email = ? , age = ?
+                WHERE id = ?
+                """;
+        int rowsAffected = jdbcTemplate.update(sql,
+                update.getName(),
+                update.getEmail(),
+                update.getAge(),
+                update.getId());
+        System.out.printf("Update Customer, rowsAffected = %d\n", rowsAffected);
+    }
+}
--- src/main/resources/db/migration/V1__Initial_Version.sql
+++
@@ -1,8 +0,0 @@
---BIGSERIAL creates a BIGINT sequence
---if now we would have to CREATE SQUENCE sequence_name;
---then swap BIGSERIAL for BIGINT DEFAULT nextval(sequence_name)
-CREATE TABLE customer(
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    age INT NOT NULL);
