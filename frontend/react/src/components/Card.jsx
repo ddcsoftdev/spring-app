b54872eab0173236c15bc59ded95159fe67b8abd
ddcsoftdev <ddcsoftwaredev@gmail.com>
2024-01-25 22:24:03 +0100

    added gender into cards

--- frontend/react/src/components/Card.jsx
+++ frontend/react/src/components/Card.jsx
@@ -14,7 +14,15 @@ import {
     useColorModeValue,
 } from '@chakra-ui/react'
 
-export default function CardWithImage({id, name, email, age}) {
+export default function CardWithImage({id, name, email, age, gender}) {
+    //adapt gender to fit "male" or "female"
+    const converGenderEntry = (gender) =>{
+        if (gender === "male"){
+            return "men"
+        } else if (gender === "female"){
+            return "women"
+        }
+    }
     return (
         <Center py={6}>
             <Box
@@ -36,11 +44,12 @@ export default function CardWithImage({id, name, email, age}) {
                     objectFit="cover"
                     alt="#"
                 />
+
                 <Flex justify={'center'} mt={-12}>
                     <Avatar
                         size={'xl'}
                         src={
-                            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+                            `https://randomuser.me/api/portraits/${converGenderEntry(gender)}/${id}.jpg`
                         }
                         css={{
                             border: '2px solid white',
@@ -55,7 +64,8 @@ export default function CardWithImage({id, name, email, age}) {
                             {name}
                         </Heading>
                         <Text color={'gray'} fontScale={"xs"}>{email}</Text>
-                        <Text color={'gray.550'}>Age {age}</Text>
+                        <Text color={'gray'}>Age {age}</Text>
+                        <Text color={'gray'}>{gender.toString().toUpperCase()}</Text>
                     </Stack>
 
                 </Box>
